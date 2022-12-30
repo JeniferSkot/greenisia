b3dbfc4960cb46d139011935e14523b9332eaea4
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-12-30 23:19:47 +0200

    Dynamic player jump height

--- src/move_player.cpp
+++ src/move_player.cpp
@@ -1,5 +1,7 @@
 #include "global.hpp"
 #include <cstdint>
+#include <cmath>
+#include <stdexcept>
 #include <SDL2/SDL_keyboard.h>
 #include "player.hpp"
 #include "map.hpp"
@@ -13,13 +15,18 @@ void move_player(int progress)
 {
     const uint8_t* keyboard = SDL_GetKeyboardState(nullptr);
 
+    // X
     if(keyboard[SDL_SCANCODE_A] || keyboard[SDL_SCANCODE_LEFT])
         player.pos.x -= player.speed * progress / 1000.0f;
 
     if(keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT])
         player.pos.x += player.speed * progress / 1000.0f;
 
+    // wall collision.
 
+    
+
+    // Y
     player.velocity += player.mass * progress / 1000.0f;
     if(player.velocity > velocity_limit)
         player.velocity = velocity_limit;
@@ -29,14 +36,31 @@ void move_player(int progress)
     if(player_foothold()) {
         player.velocity = 0;
         int player_bottom = player.pos.y + player.size.y;
-        int foothold_block = player_bottom / block_size.y;
-        player_bottom = foothold_block * block_size.y;
+        int player_center = player.pos.x + player.size.x / 2;
+
+        int foothold_block_x = player_center / block_size.x;
+        int foothold_block_y = player_bottom / block_size.y;
+        
+        try {
+            Block block = map.at(foothold_block_x,
+                                 foothold_block_y - 1);
+            if(block != B_AIR)
+                foothold_block_y--;
+        } catch (std::runtime_error e) {
+            // Ignore this error :D
+            // We don't want block collision outside of the map
+        }
+
+        player_bottom = foothold_block_y * block_size.y;
         player.pos.y = player_bottom - player.size.y;
 
 
         if(keyboard[SDL_SCANCODE_W] || keyboard[SDL_SCANCODE_UP])
             player.velocity = -player.jump_power;
-    }
+
+    } else if(!keyboard[SDL_SCANCODE_W] && !keyboard[SDL_SCANCODE_UP])
+        if(player.velocity < 0)
+            player.velocity /= pow(1.03, progress);
 }
 
 bool player_foothold()
