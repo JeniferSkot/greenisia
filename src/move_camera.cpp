2e530e2f96712da9a498a7331f790d8fcf9aa4dc
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-12-31 19:43:35 +0200

    Limiting camera to positive coordinates

--- src/move_camera.cpp
+++ src/move_camera.cpp
@@ -8,4 +8,10 @@ void move_camera(int progress)
                 - camera.w / 2;
     camera.y = player.pos.y + player.size.y / 2
                 - camera.h / 2;
+
+    if(camera.x < 0)
+        camera.x = 0;
+
+    if(camera.y < 0)
+        camera.y = 0;
 }
