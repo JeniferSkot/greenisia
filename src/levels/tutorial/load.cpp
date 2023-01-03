48bbff4f82b3397efe52f5d1f31aec6678798a09
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-01-03 23:41:35 +0200

    levels/tutorial: Initial camera position on load

--- src/levels/tutorial/load.cpp
+++ src/levels/tutorial/load.cpp
@@ -1,14 +1,20 @@
 #include "levels/tutorial.hpp"
+#include "global.hpp"
 #include "level.hpp"
 #include "player.hpp"
+#include "camera.hpp"
 
 
 void tutorial::load()
 {
     current_level = &level;
 
-    player.pos.x = 100;
-    player.pos.y = 100;
+    player.pos.x = block_size.x * 2;
+    player.pos.y = block_size.y * 17 - player.size.y;
+
+    auto cam_pos = get_camera_target();
+    camera.x = cam_pos.x;
+    camera.y = cam_pos.y;
 
     load_backgrounds(&level);
 }
