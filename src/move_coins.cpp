fcedded3429217dde99702d1abbabff509f69e1d
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-02-07 20:32:25 +0200

    fix: Collecting coins on staircases

--- src/move_coins.cpp
+++ src/move_coins.cpp
@@ -255,7 +255,7 @@ void coin_player_collision(Coin& coin)
 static bool has_player_collision(Coin const& coin)
 {
     auto const& pos = coin.pos;
-    const int range = block_size.x;
+    const int range = block_size.x * 2;
     if(pos.x < player.pos.x + player.size.x)
     if(pos.x + pos.w > player.pos.x)
     if(pos.y < player.pos.y + player.size.y + range)
