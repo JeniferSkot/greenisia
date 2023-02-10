0dc191179c677b3aff58a17b4ae7ed4c5ce0e339
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-02-10 11:06:37 +0200

    level: Adding inital entity data
    Currently only player and items

--- src/level.hpp
+++ src/level.hpp
@@ -3,19 +3,39 @@
 
 #include "map.hpp"
 #include "background.hpp"
+#include "story.hpp"
+#include "item.hpp"
 #include <vector>
 #include <string>
+#include <SDL2/SDL_rect.h>
 
 using std::vector;
 using std::string;
 
 
+struct ItemState
+{
+    fs::path item;
+    SDL_Point pos;
+
+    fs::path story_cue;
+};
+
+struct EntityData
+{
+    SDL_Point player_pos {0, 0};
+    vector<ItemState> items;
+};
+
 struct Level
 {
     string map_file;
+    string entity_data_file;
 
     Map map;
     vector<Background> backgrounds;
+
+    EntityData entity_data;
 };
 
 
@@ -23,5 +43,9 @@ extern Level* current_level;
 
 void init_levels();
 void load_backgrounds(Level*);
+
+
+EntityData load_entity_data(fs::path);
+
 
 #endif // INCLUDE_LEVEL_HPP
