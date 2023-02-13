09d06692d3246e23474b0a7eafbebe99e6e35b7a
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-02-13 19:12:51 +0200

    text: Adding render_text and render_text_cached

--- src/text.cpp
+++ src/text.cpp
@@ -1,7 +1,10 @@
 #include "text.hpp"
+#include "render.hpp"
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_render.h>
 #include <iostream>
 #include <string>
+#include <map>
 
 using std::cout;
 using std::endl;
@@ -13,7 +16,7 @@ TTF_Font* font()
     static TTF_Font* font = [](){
         const string path = "assets/fonts/"
             "comic_shanns_2.ttf";
-        const int size = 16;
+        const int size = 18;
 
         cout << "Loading font " << path << endl;
         
@@ -28,3 +31,66 @@ TTF_Font* font()
 
     return font;
 }
+
+
+SDL_Texture* render_text(string const& text,
+                         SDL_Point* size)
+{
+    const SDL_Color white {255, 255, 255, 255};
+    auto surface = TTF_RenderUTF8_Blended
+        (font(), text.c_str(), white);
+
+    if(surface == nullptr) {
+        if(size) {
+            size->x = 0;
+            size->y = 0;
+        }
+        return nullptr;
+    }
+
+    if(size) {
+        size->x = surface->w;
+        size->y = surface->h;
+    }
+
+
+    SDL_Texture* texture
+        = SDL_CreateTextureFromSurface(rnd, surface);
+
+    if(texture == nullptr) {
+        cout << "Can not create text texture: "
+             << text << endl;
+        cout << SDL_GetError() << endl;
+    }
+
+    SDL_FreeSurface(surface);
+
+    return texture;
+}
+
+SDL_Texture* render_text_cached(string const& text,
+                                SDL_Point* size)
+{
+    struct CData
+    {
+        SDL_Texture* texture = nullptr;
+        SDL_Point size {0, 0};
+    };
+
+    static std::map<string, CData> cache;
+
+    auto itr = cache.find(text);
+    if(itr != cache.end()) {
+        if(size)
+            *size = itr->second.size;
+        return itr->second.texture;
+    }
+
+    auto& data  = cache[text];
+    data.texture = render_text(text, &data.size);
+
+    if(size)
+        *size = data.size;
+    return data.texture;
+}
+
--- src/text.hpp
+++ src/text.hpp
@@ -2,7 +2,16 @@
 #define INCLUDE_TEXT_HPP
 
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_rect.h>
+#include <string>
+
+struct SDL_Texture;
 
 TTF_Font* font();
+
+SDL_Texture* render_text(std::string const& text,
+                         SDL_Point* size = 0);
+SDL_Texture* render_text_cached(std::string const& text,
+                                SDL_Point* size = 0);
 
 #endif // INCLUDE_TEXT_HPP
